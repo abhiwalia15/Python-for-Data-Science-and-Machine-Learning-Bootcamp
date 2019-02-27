7286b79f40ebd78e68dc6f478bad01a24e45c4fa
Mrinal <waliamrinal@gmail.com>
2019-02-27 23:36:29 +0530

    pride and prejudice book natural language proecessing python file

---
+++ Machine Learning/Natural-Language-Processing/Web Scrapping with NLP/PrideAndPrejudice_book_nltk.py
@@ -0,0 +1,42 @@
+import re
+from bs4 import BeautifulSoup 
+import requests
+from nltk.tokenize import RegexpTokenizer
+import seaborn as sns
+
+
+
+def plot_word_freq(url):
+    """Takes a url (from Project Gutenberg) and plots a word frequency
+    distribution"""
+    # Make the request and check object type
+    r = requests.get(url)
+    # Extract HTML from Response object and print
+    html = r.text
+    # Create a BeautifulSoup object from the HTML
+    soup = BeautifulSoup(html, "html5lib")
+    # Get the text out of the soup and print it
+    text = soup.get_text()
+    # Create tokenizer
+    tokenizer = RegexpTokenizer('\w+')
+    # Create tokens
+    tokens = tokenizer.tokenize(text)
+    # Initialize new list
+    words = []
+    # Loop through list tokens and make lower case
+    for word in tokens:
+        words.append(word.lower())
+    # Get English stopwords and print some of them
+    sw = nltk.corpus.stopwords.words('english')
+    # Initialize new list
+    words_ns = []
+    # Add to words_ns all words that are in words but not in sw
+    for word in words:
+        if word not in sw:
+            words_ns.append(word)
+    # Create freq dist and plot
+    freqdist1 = nltk.FreqDist(words_ns)
+    print(freqdist1.plot(25))
+    
+plot_word_freq('https://www.gutenberg.org/files/42671/42671-h/42671-h.htm')
+    
