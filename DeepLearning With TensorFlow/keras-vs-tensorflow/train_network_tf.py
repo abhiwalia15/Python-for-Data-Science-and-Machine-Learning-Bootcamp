5bdc39d763b15cecd74347df002091cb6ba6b055
Mrinal <waliamrinal@gmail.com>
2019-05-16 19:21:59 +0530

    training the tenserflow model

---
+++ DeepLearning With TensorFlow/keras-vs-tensorflow/train_network_tf.py
@@ -0,0 +1,76 @@
+# USAGE
+# python train_network_tf.py
+
+# set the matplotlib backend so figures can be saved in the background
+import matplotlib
+matplotlib.use("Agg")
+
+# import the necessary packages
+from pyimagesearch.minivggnettf import MiniVGGNetTF
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+import argparse
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--plot", type=str, default="plot_tf.png",
+	help="path to output loss/accuracy plot")
+args = vars(ap.parse_args())
+
+# load the training and testing data, then scale it into the
+# range [0, 1]
+print("[INFO] loading CIFAR-10 data...")
+split = tf.keras.datasets.cifar10.load_data()
+((trainX, trainY), (testX, testY)) = split
+trainX = trainX.astype("float") / 255.0
+testX = testX.astype("float") / 255.0
+
+# convert the labels from integers to vectors
+lb = LabelBinarizer()
+trainY = lb.fit_transform(trainY)
+testY = lb.transform(testY)
+
+# initialize the label names for the CIFAR-10 dataset
+labelNames = ["airplane", "automobile", "bird", "cat", "deer",
+	"dog", "frog", "horse", "ship", "truck"]
+
+# initialize the initial learning rate, total number of epochs to
+# train for, and batch size
+INIT_LR = 0.01
+EPOCHS = 30
+BS = 32
+
+# initialize the optimizer and model
+print("[INFO] compiling model...")
+opt = tf.keras.optimizers.SGD(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+model = MiniVGGNetTF.build(width=32, height=32, depth=3,
+	classes=len(labelNames))
+model.compile(loss="categorical_crossentropy", optimizer=opt,
+	metrics=["accuracy"])
+
+# train the network
+print("[INFO] training network for {} epochs...".format(EPOCHS))
+H = model.fit(trainX, trainY, validation_data=(testX, testY),
+	batch_size=BS, epochs=EPOCHS, verbose=1)
+
+# evaluate the network
+print("[INFO] evaluating network...")
+predictions = model.predict(testX, batch_size=32)
+print(classification_report(testY.argmax(axis=1),
+	predictions.argmax(axis=1), target_names=labelNames))
+
+# plot the training loss and accuracy
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, EPOCHS), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, EPOCHS), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, EPOCHS), H.history["acc"], label="train_acc")
+plt.plot(np.arange(0, EPOCHS), H.history["val_acc"], label="val_acc")
+plt.title("Training Loss and Accuracy on Dataset")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend(loc="lower left")
+plt.savefig(args["plot"])
